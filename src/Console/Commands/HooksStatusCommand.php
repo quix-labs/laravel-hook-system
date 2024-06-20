c8498d863f852e2e34d56d979e35c57b1d22e7b5
Alan Colant <alan.colant@outlook.fr>
2024-06-20 18:44:37 +0200

    GetHooksTable is not fully cached to avoid hooks:status to appear bad

--- src/Console/Commands/HooksStatusCommand.php
+++ src/Console/Commands/HooksStatusCommand.php
@@ -4,6 +4,7 @@ namespace QuixLabs\LaravelHookSystem\Console\Commands;
 
 use Illuminate\Console\Command;
 use QuixLabs\LaravelHookSystem\Facades\HookManager;
+use QuixLabs\LaravelHookSystem\Hook;
 use QuixLabs\LaravelHookSystem\Hooks\GetHooksTable;
 use QuixLabs\LaravelHookSystem\Utils\CommandTable;
 
@@ -19,7 +20,27 @@ class HooksStatusCommand extends Command
             $this->components->warn('Hooks are actually cached!');
         }
 
-        $rows = [];
+        $hooks = collect(HookManager::getHooks())
+            ->mapWithKeys(fn (string|Hook $hook) => [$hook => HookManager::getInterceptorsForHook($hook)])->toArray();
+        $rows = collect($hooks)->map(function (array $interceptors, string $hookClass) {
+            $callables = collect($interceptors)
+                ->map(fn (array $callables, int $priority) => implode('<br/>', array_map(
+                    fn (callable $callable) => static::_callableToString($callable), $callables)
+                ))->join('<br/>');
+
+            $priorities = collect($interceptors)
+                ->map(fn (array $callables, int $priority) => implode('<br/>', array_fill(0, count($callables), $priority)))
+                ->join('<br/>');
+
+            return [
+                'Hook' => $hookClass,
+                'Interceptors' => $callables,
+                'Priority' => $priorities,
+                'Fully Cacheable' => HookManager::isFullyCacheable($hookClass) ? 'YES' : 'NO',
+                'Fully Cached' => HookManager::isFullyCached($hookClass) ? 'YES' : 'NO',
+            ];
+        })->toArray();
+
         GetHooksTable::send($rows);
         if (count($rows) > 0) {
             $this->output->writeln(CommandTable::asString($rows));
@@ -28,5 +49,24 @@ class HooksStatusCommand extends Command
         }
 
         return self::SUCCESS;
+    }
+
+    public static function _callableToString(callable $callable): string
+    {
+        if (is_array($callable)) {
+            $class = is_object($callable[0]) ? get_class($callable[0]) : $callable[0];
+            $method = $callable[1];
+        } elseif (is_string($callable) && str_contains($callable, '::')) {
+            [$class, $method] = explode('::', $callable);
+        } else {
+            $class = get_class($callable);
+            $method = '__invoke';
+        }
+
+        $reflection = new \ReflectionMethod($class, $method);
+        $line = $reflection->getStartLine();
+
+        return sprintf('%s@%s:%d', $class, $method, $line);
+
     }
 }
